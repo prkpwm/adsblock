a76df7bd3dd30d41734b49fce5a483e227d83d41
prkpwm <78756457+prkpwm@users.noreply.github.com>
2023-01-15 01:02:52 +0700

    block url

--- contentScript.js
+++ contentScript.js
@@ -1,13 +1,17 @@
 
 if (typeof init === 'undefined') {
-    let BLOCKED = [
+    let BLOCKED_ELEMENT = [
         '[class*="ads"]',
         '[id*="ads"]',
     ];
 
+    let BLOCKED_URL = [
+        "animekimi.com",
+    ]
+
     const deep_iframe = (iframe) => {
         try {
-            BLOCKED.forEach((selector) => {
+            BLOCKED_ELEMENT.forEach((selector) => {
                 const elements = iframe?.contentWindow?.document?.body?.querySelectorAll(selector);
                 elements?.forEach((element) => {
                     element.style.display = 'none';
@@ -23,12 +27,23 @@ if (typeof init === 'undefined') {
 
 
     const init = () => {
+
+
+
         const debouncedRemoveElements = () => {
-            chrome?.storage?.local?.get(['key'], function (result) {
-                if (result.key) BLOCKED = result.key;
-                else chrome?.storage?.local?.set({ 'key': BLOCKED }, function () { });
-            });
-            BLOCKED.forEach((selector) => {
+            try {
+                chrome?.storage?.local?.get(['key'], function (result) {
+                    if (result.key) BLOCKED_ELEMENT = result.key;
+                    else chrome?.storage?.local?.set({ 'key': BLOCKED_ELEMENT }, () => { });
+                });
+                chrome?.storage?.local?.get(['url'], function (result) {
+                    if (result.url) BLOCKED_URL = result.url;
+                    else chrome?.storage?.local?.set({ 'url': BLOCKED_URL }, () => { });
+                });
+            }
+            catch (e) { }
+
+            BLOCKED_ELEMENT.forEach((selector) => {
                 const elements = document.querySelectorAll(selector);
                 elements?.forEach((element) => {
                     element.style.display = 'none';
@@ -41,9 +56,15 @@ if (typeof init === 'undefined') {
             }
         }
         try {
+            BLOCKED_URL.forEach((url) => {
+                if (window.location.href.includes(url)) { window.location.href = 'https://www.google.com/'; } else { }
+            });
             setInterval(debouncedRemoveElements, 1000);
         } catch (e) { }
     }
+
+
+
     init();
 }
 
--- popup.js
+++ popup.js
@@ -3,6 +3,10 @@ let BLOCKED = [
     '[id*="ad"]',
 ];
 
+let BLOCKED_URL = [
+    "animekimi.com",
+]
+
 
 const addEl = (value) => {
     const el = document.createElement('div')
@@ -10,6 +14,8 @@ const addEl = (value) => {
     const input = document.createElement('input')
     input.type = 'text'
     input.value = value
+    input.style.backgroundColor = 'white'
+    input.style.color = 'black'
     el.appendChild(input)
     const button = document.createElement('button')
     button.innerText = 'X'
@@ -17,12 +23,36 @@ const addEl = (value) => {
     button.addEventListener('click', () => {
         const index = BLOCKED.indexOf(value)
         BLOCKED.splice(index, 1)
-        chrome?.storage?.local.set({ 'key': BLOCKED }, function () { });
+        chrome?.storage?.local.set({ 'key': BLOCKED }, () => { });
         el.remove()
     })
     el.appendChild(button)
     document.body.appendChild(el)
 }
+
+const addUrl = (value) => {
+    const el = document.createElement('div')
+    el.className = 'content'
+    const input = document.createElement('input')
+    input.type = 'text'
+    input.value = value
+    input.style.backgroundColor = 'black'
+    input.style.color = 'white'
+    el.appendChild(input)
+
+    const button = document.createElement('button')
+    button.innerText = 'X'
+    button.className = 'remove'
+    button.addEventListener('click', () => {
+        const index = BLOCKED_URL.indexOf(value)
+        BLOCKED_URL.splice(index, 1)
+        chrome?.storage?.local.set({ 'url': BLOCKED_URL }, () => { });
+        el.remove()
+    })
+    el.appendChild(button)
+    document.body.appendChild(el)
+}
+
 
 const onAddEl = () => {
     const el = document.createElement('div')
@@ -37,25 +67,58 @@ const onAddEl = () => {
     button.addEventListener('click', () => {
         const index = BLOCKED.indexOf(input.value)
         BLOCKED.splice(index, 1)
-        chrome?.storage?.local.set({ 'key': BLOCKED }, function () { });
+        chrome?.storage?.local.set({ 'key': BLOCKED }, () => { });
         el.remove()
     })
 
     input.addEventListener('change', () => {
         if (!BLOCKED?.find((value) => value === input.value)) {
             BLOCKED.push(input.value)
-            chrome?.storage?.local.set({ 'key': BLOCKED }, function () { });
+            chrome?.storage?.local.set({ 'key': BLOCKED }, () => { });
         }
     })
     el.appendChild(button)
     document.body.appendChild(el)
 }
 
-const init = () => {
+const onAddUrl = () => {
+    const el = document.createElement('div')
+    el.className = 'content'
+    const input = document.createElement('input')
+    input.type = 'text'
+    input.placeholder = 'Enter URL'
+    el.appendChild(input)
+    const button = document.createElement('button')
+    button.innerText = 'X'
+    button.className = 'remove'
+    button.addEventListener('click', () => {
+        const index = BLOCKED_URL.indexOf(input.value)
+        BLOCKED_URL.splice(index, 1)
+        chrome?.storage?.local.set({ 'url': BLOCKED_URL }, () => { });
+        el.remove()
+    })
+
+    input.addEventListener('change', () => {
+        if (!BLOCKED_URL?.find((value) => value === input.value)) {
+            BLOCKED_URL.push(input.value)
+            chrome?.storage?.local.set({ 'url': BLOCKED_URL }, () => { });
+        }
+    })
+    el.appendChild(button)
+    document.body.appendChild(el)
+}
+
+const initEl = () => {
     const flexRow = document.createElement('div')
     flexRow.className = 'flex-row'
     const h1 = document.createElement('h1')
     h1.innerText = 'BLOCKED LIST'
+    h1.style.width = '100%'
+    h1.style.backgroundColor = 'white'
+    h1.style.color = 'black'
+    h1.style.lineHeight = '50px'
+    h1.style.border = '1px solid black'
+    h1.style.margin = '0.5rem 0'
     flexRow.appendChild(h1)
     const div = document.createElement('div')
     const button = document.createElement('button')
@@ -69,12 +132,48 @@ const init = () => {
 
     chrome?.storage?.local?.get(['key'], function (result) {
         if (result.key) BLOCKED = result.key;
-        else chrome.storage.local.set({ 'key': BLOCKED }, function () { });
+        else chrome.storage.local.set({ 'key': BLOCKED }, () => { });
         BLOCKED?.forEach((value) => {
             addEl(value)
         })
     });
+}
 
+const initUrl = () => {
+    const flexRow2 = document.createElement('div')
+    flexRow2.className = 'flex-row'
+    const h2 = document.createElement('h1')
+    h2.innerText = 'BLOCKED URL'
+    h2.style.backgroundColor = 'black'
+    h2.style.color = 'white'
+    h2.style.width = '100%'
+    h2.style.lineHeight = '50px'
+    h2.style.margin = '0.5rem 0'
+    flexRow2.appendChild(h2)
+    const div2 = document.createElement('div')
+    const button2 = document.createElement('button')
+    button2.className = 'add'
+    const innerText2 = document.createElement('div')
+    innerText2.innerText = 'X'
+    button2.appendChild(innerText2)
+    button2.addEventListener('click', onAddUrl)
+    flexRow2.appendChild(button2)
+    document.body.appendChild(flexRow2)
+
+    chrome?.storage?.local?.get(['url'], function (result) {
+        if (result.url) BLOCKED_URL = result.url;
+        else chrome.storage.local.set({ 'url': BLOCKED_URL }, () => { });
+        BLOCKED_URL?.forEach((value) => {
+            addUrl(value)
+        })
+    });
+}
+
+
+
+const init = () => {
+    initEl()
+    initUrl()
 }
 
 

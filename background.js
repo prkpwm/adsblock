036bf68519735f7bb6dde3814bee8ecb5dde6832
prkpwm <78756457+prkpwm@users.noreply.github.com>
2023-01-31 21:33:52 +0700

    fix bug

--- background.js
+++ background.js
@@ -1,18 +1,15 @@
 chrome.tabs.onUpdated.addListener((tabId, tab) => {
-  if (tab.BLOCKED_URL && tab.BLOCKED_URL) {
+  if (tab.url && tab.url.indexOf('chrome://') == -1) {
     chrome?.storage?.local?.get(['BLOCKED_URL'], function (result) {
       if (result.BLOCKED_URL) {
         const BLOCKED_URL = result.BLOCKED_URL;
         for (const url of BLOCKED_URL) {
-          if (url.status && tab.BLOCKED_URL.match(url.name)) {
+          if (url.status && tab.url.match(url.name)) {
             chrome.tabs.update(tabId, { url: 'https://www.google.com' })
           }
         }
       }
     });
-  }
-
-  if (tab.BLOCKED_URL && tab.BLOCKED_URL.indexOf('chrome://') == -1) {
     chrome.scripting?.executeScript({
       files: ['contentScript.js'],
       target: { tabId: tab.id }
